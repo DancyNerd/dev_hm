3a249225484c8db545c30e22df4a9c365d448601
JD <72483040+DancyNerd@users.noreply.github.com>
2021-02-06 20:17:43 -0500

    NUKED
    Entire front end nuked. Troubleshooting with just one tiny piece of it. Going to yeet laptop through window. Please send coffee.

---
+++ HM_dev/myapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import loginform from './loginform';
+
+function App() {
+  return (
+    <div className="App">
+      {loginform}
+    </div>
+  );
+}
+
+export default App;
