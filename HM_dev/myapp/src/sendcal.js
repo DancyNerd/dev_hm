0ac2da4c2899294f661a3d93ec02fc7f7a36e7b7
JD <72483040+DancyNerd@users.noreply.github.com>
2021-09-15 19:51:50 -0400

    API Call Updates
    Added beginning of API calls to sendcal.js

--- HM_dev/myapp/src/sendcal.js
+++ HM_dev/myapp/src/sendcal.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import axios from 'axios';
+import { ActionSheetIOS } from 'react-native';
 
 //This file is for interaction with the FoodCentral API.
 
-function apiCall() {
+function ApiCall() {
+    var apiUrl = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=ZDTgbZ7bnPCNIQbmIOjKRIzJ4S5jFEeTvvYwDYHB&query=";
+    var searchParam = "swiss%20cheese"; //placeholder
+    var sendUrl = apiUrl+searchParam
+    axios.get(apiUrl)
+        .then(response=> (this.info = response.data.bpi))
+        .catch(error=> console.log(error))
 
 }
 
-export default apiCall;
+export default ApiCall;
