a021c5e9337312b59eb37bd8d7935fd9bf78b602
JD <72483040+DancyNerd@users.noreply.github.com>
2022-03-18 14:58:57 -0400

    Created Skeleton of Calorie Return FE
    Mostly a bunch of notes, honestly
    Created the first function
    I'm supposed to be off this weekend, but didn't touch this all week, so we have this yay

---
+++ HM_dev/myapp/src/callistret.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+/*
+This should be in the form of a list that can be selected from.
+-drop down or all at once??
+-send selection back to python back end
+-makes selection and returns calorie results
+-probably needs to go in some kind of db for graphing purposes
+-consider storing per day and updating quantities of that day until the day is over
+-graph plots by day
+*/
+
+function CalRet({}) {
+    return(
+        <h4>
+            //return stuff goes here
+        </h4>
+    )
+}
