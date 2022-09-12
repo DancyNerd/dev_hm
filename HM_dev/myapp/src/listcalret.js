c022ed2518b473347356be48174ce9c769192153
JD <72483040+DancyNerd@users.noreply.github.com>
2022-09-11 21:58:51 -0400

    Replacing listcalret.js for loop
    Replacing for loop on line 11 with map
    Incomplete/working with Torhal

--- HM_dev/myapp/src/listcalret.js
+++ HM_dev/myapp/src/listcalret.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 
 //Returns list of selectable options based on user's calorie input.
 
-function CalRet ({ results }) {
-    console.log(results);
+function CalRet ({ results}) {
+
+    var dispRes = [];
+
+    for(let i=0; i<results.length; i++) {
+ 
+        console.log(results[i]);
+        dispRes[i] = results[i][1];
+    }
+
     return (
         <p>
-            Found it! {results.map(function(d, idx){
-                return(<li key={idx}>{d.name}</li>)
+            Found it! {dispRes.map(function(d, ind){
+                return(<li key={ind}>{d.name}</li>)
             })}
         </p>
     );
 }
 
+/*
+Array within an array is occurring.
+Sort them into smaller arrays.
+Examine the possibilities of using map or just calling by number.
+*/
 
 export default CalRet;
