008b1aa3e451ae8d01927ff126cb3ceecfa22863
JD <72483040+DancyNerd@users.noreply.github.com>
2022-09-12 20:47:04 -0400

    state changes to listcalret.js
    Added functions, variables for listcalret.js

--- HM_dev/myapp/src/listcalret.js
+++ HM_dev/myapp/src/listcalret.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-boostrap/Dropdown';
+import axios from 'axios';
 
 //Returns list of selectable options based on user's calorie input.
 
@@ -8,15 +9,31 @@ class CalRet extends React.Component {
         super(props);
 
         this.state = {
-            
+            dispVal: '',
+            selection: ''
         }
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSelect = this.handleSelect.bind(this);
 
     }
 
+    handleChange(event) {
+        const dispVal = event.dispVal;
 
+        this.setState({
+            ...this.state,
+            [name]:value
+        });
+    }
     
+    handleSelect(selection) {
+        event.preventDefault();
+        const selection = this.state;
+
+        const floute = "http://127.0.0.1:5000/"
+    }
+
 }
 
 
